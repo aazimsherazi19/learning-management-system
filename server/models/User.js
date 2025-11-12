d6770114d1e265c40affbbec750851ecf62ead0d
aazim_sherazi <aazimsherazi3210@gmail.com>
2025-11-12 14:02:21 +0500

    Backend Initial Setup

---
+++ server/models/User.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    _id: {type: String, required: true},
+    name: {type: String, required: true, min: 3},
+    email: {type: String, required: true},
+    password: {type: String, required: true, min: 8},
+    imageUrl: {type: String, required: true},
+    enrolledCourses: [
+        {type: mongoose.Schema.Types.ObjectId,
+            ref: 'Course'
+        }
+    ],
+}, {timestamps: true});
+
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+
+export default User;
---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv/config';
+import connectDB from './configs/mongodb.js';
+import { clerkwebhooks } from './controllers/webhooks.js';
+
+// Initialize app
+const app = express();
+connectDB();
+
+// Middlewares
+app.use(cors());
+
+// Middlewares
+app.get('/', (req, res)=>{
+  res.send("API Working");
+})
+app.post('/clerk', express.json(), clerkwebhooks);
+
+// PORT
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=>{
+    console.log("Server is running on Port: " + PORT);
+});
